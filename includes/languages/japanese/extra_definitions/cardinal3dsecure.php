fc0865d33303e762d4f9e574699cd7e59d6d2472
yama <yamamoto@kyms.ne.jp>
2012-04-21 17:22:09 +0900

    @authorタグ(DocBlock)を埋めてみました

--- includes/languages/japanese/extra_definitions/cardinal3dsecure.php
+++ includes/languages/japanese/extra_definitions/cardinal3dsecure.php
@@ -5,6 +5,7 @@
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version $Id: cardinal3dsecure.php 15757 2010-03-31 19:13:24Z drbyte $
+ * @author yama
  */
   define('WARNING_SESSION_TIMEOUT', 'We are sorry, but your login session has expired. Your shopping selections have been saved.<br /><br />If visible, please click the Home link above, and login again to see your cart contents and complete checkout.<br /><br />Alternatively, if you arrived on this page by using the Back Button in your browser, please browse forward again to access the Home link. Thanks!');
   define('ERROR_PAYMENT_FAILURE_TEXT', 'Unable to complete payment, please enter another form of payment.');
