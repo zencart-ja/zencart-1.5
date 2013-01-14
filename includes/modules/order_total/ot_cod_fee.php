a7642ce89d54ac399b86696534e012d719942807
yama <yamamoto@kyms.ne.jp>
2013-01-14 18:12:14 +0900

    Thomas Plänkers氏のクレジット文字化けを修正

--- includes/modules/order_total/ot_cod_fee.php
+++ includes/modules/order_total/ot_cod_fee.php
@@ -5,7 +5,7 @@
  * @package orderTotal
  * @copyright Copyright 2003-2009 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
- * @copyright Portions Copyright (c) 2002 Thomas Pl・ｽnkers http://www.oscommerce.at
+ * @copyright Portions Copyright (c) 2002 Thomas Plänkers http://www.oscommerce.at
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version $Id: ot_cod_fee.php 14820 2009-11-15 16:17:45Z ajeh $
  * @author obitastar
